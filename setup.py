f05057b4eaff48bd50dac80e93985a997a85f334
Everaldo Canuto <everaldo.canuto@gmail.com>
2021-08-29 01:59:17 -0300

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="columba",
-    version="0.2.2",
+    version="0.3.0",
     description="A set of Google Colab utilities, recipes and snippets",
     author="Everaldo Canuto",
     author_email="everaldo.canuto@gmail.com",
